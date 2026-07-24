2fe83edd0abdcff0dc3b208bb787ce0764327e7c
arunofhyd <arunthomas04042001@gmail.com>
2026-07-24 11:37:30 +0530

    Initial Cask formula for jobsmonitor v2.0.0

---
+++ Casks/jobsmonitor.rb
@@ -0,0 +1,29 @@
+cask "jobsmonitor" do
+  version "2.0.0"
+  sha256 "5cc1f65fba8e516eae1e4a798ea07fca4f81c3df28bea96a7d9498766e9ea56d"
+
+  url "https://github.com/arunofhyd/JobsMonitor/releases/download/v#{version}/JobsMonitor.zip"
+  name "Jobs Monitor"
+  desc "Native macOS menu bar app for Apple job postings"
+  homepage "https://github.com/arunofhyd/JobsMonitor"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/arunofhyd/JobsMonitor/main/version.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "JobsMonitor.app"
+
+  uninstall quit:      "com.aoh.jobsmonitor",
+            launchctl: "com.aoh.jobsmonitor"
+
+  zap trash: [
+    "~/Library/Application Support/JobsMonitor",
+    "~/Library/Logs/JobsMonitor",
+    "~/Library/Preferences/com.aoh.jobsmonitor.plist",
+  ]
+end
